28a2c8a6cabe6d7fc02114553f4a92024773209d
Theodore Kim <teddy@multicorder.io>
2018-02-22 18:45:21 -0500

    First Email

--- routes/study.js
+++ routes/study.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var emails = require('../data/emails.json');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
 	if (!req.cookies.round) { 
@@ -8,7 +10,14 @@ router.get('/', (req, res, next) => {
 	}
 	else {
 		if (parseInt(req.cookies.round) < 10) 
-			res.render('study');
+			res.render('study', {
+				message:emails[0][0].phishing.message,
+				subject:"Hello World",
+				toline:"you, me",
+				fro: "Teddy Kim",
+				faddress: "teddy@teddy.com",
+				timestamp: "Feb 13 (ten days ago)"
+			});
 		else
 			res.redirect('/endsurvey');
 	}
